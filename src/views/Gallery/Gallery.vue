8cce1eb14eb799785bdab8e643174b088dd0d523
straw <2053662445@qq.com>
2022-07-17 23:17:53 +0800

    完成了pc端画廊

--- src/views/Gallery/Gallery.vue
+++ src/views/Gallery/Gallery.vue
@@ -9,53 +9,53 @@
 
   <div class="gallery_container">
     <div class="gallery_container_nav">
-      <span>你的画廊</span>
+      <div class="upload_img">
+        <span>你的画廊</span>
+        <span>
+          <!--          <img src="https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg" alt="" />-->
+          <upload-picture class="img" theme="outline" size="32" fill="#080808" />
+          点击上传
+        </span>
+      </div>
+      <p>美妙的世界</p>
       <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-        <el-tab-pane label="默认分类" name="first">
-          <Waterfall :list="lists"></Waterfall>
-        </el-tab-pane>
-        <el-tab-pane label="动漫二次元" name="second">
-          <Waterfall :list="lists"></Waterfall>
-        </el-tab-pane>
-        <el-tab-pane label="风景" name="third">
-          <Waterfall :list="lists"></Waterfall>
-        </el-tab-pane>
-        <el-tab-pane label="人物" name="fourth">
-          <Waterfall :list="lists"></Waterfall>
-        </el-tab-pane>
-        <el-tab-pane label="美女" name="fourth">
-          <Waterfall :list="lists"></Waterfall>
-        </el-tab-pane>
+        <el-tab-pane label="默认分类" name="one" />
+        <el-tab-pane label="动漫二次元" name="two" />
+        <el-tab-pane label="风景" name="three" />
+        <el-tab-pane label="美女" name="fore" />
+        <el-tab-pane label="人物" name="five" />
+        <el-tab-pane label="背景" name="six" />
       </el-tabs>
     </div>
+    <Waterfall :list="lists"></Waterfall>
   </div>
 </template>
 
 <script lang="ts" setup>
+// 引入icon
+import { UploadPicture } from '@icon-park/vue-next'
 import Waterfall from '@/components/Waterfall/Waterfall.vue'
 import type { TabsPaneContext } from 'element-plus'
 
-// 标签页
-const activeName = ref('first')
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event)
-}
-
-const lists = reactive([
-  {
-    id: 1,
-    name: '美女',
-    url: 'https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg'
-  },
+let lists = ref([
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/ISntcwQY9yDJHO8.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/En9HsJif5k2lW3c.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' }
+])
+const arr = ref([
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg' },
-  {
-    id: 1,
-    name: '美女',
-    url: 'https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg'
-  },
+  { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/NiHVRvpulDWtzn8.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/ISntcwQY9yDJHO8.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/En9HsJif5k2lW3c.jpg' },
@@ -68,6 +68,23 @@ const lists = reactive([
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/opuKNlzxgQVAk9S.jpg' },
   { id: 1, name: '美女', url: 'https://i.loli.net/2021/10/02/FCwPIVi4oqYlGUH.jpg' }
 ])
+
+// 标签页
+let he = reactive({
+  id: 1,
+  name: 'zhjansgan'
+})
+
+const activeName = ref('one')
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+  console.log(tab.paneName)
+  const a = event.target
+  console.log(a.innerText)
+  lists.value = arr.value
+  console.log(lists)
+  he.id = 2
+}
 </script>
 
 <style lang="less" scoped>
