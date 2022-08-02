2522bdebaf01784fe9a1c243e533d108acbbd653
straw <2053662445@qq.com>
2022-08-02 22:00:07 +0800

    完成了资源库

---
+++ src/api/resource.ts
@@ -0,0 +1,36 @@
+import request from '@/Utils/request'
+
+/**
+ * 修改用户头像
+ * @param {String} type_one - 资源分类一级
+ * @param {String} type_two - 资源分类二级
+ * @param {String} url - 资源地址
+ * @param {String} intrudoce - 资源介绍
+ * @param {String} logourl - logo
+ * @param {String} resourcename - 资源名称
+ * @returns Promise
+ */
+export const uploadResources = ({
+  type_one,
+  type_two,
+  url,
+  intrudoce,
+  logourl,
+  resourcename
+}: any) => {
+  return request('/rice/uploadresource', 'post', {
+    type_one,
+    type_two,
+    url,
+    intrudoce,
+    logourl,
+    resourcename
+  })
+}
+/**
+ * 获取资源列表
+ * @returns Promise
+ */
+export const getResources = () => {
+  return request('/rice/getresource', 'get', {})
+}
--- src/views/Resource/Resource.vue
+++ src/views/Resource/Resource.vue
@@ -7,16 +7,32 @@
 <template>
   <SecondaryBg></SecondaryBg>
   <div class="resource_container">
-    <div class="box_container">
-      <h2>学习资源</h2>
+    <div class="add_resource">
+      <el-input v-model="input_url" placeholder="资源url" />
+      <el-select v-model="input_type" class="m-2" placeholder="选择分类" size="large">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+      <el-input v-model="input_name" placeholder="资源名称" />
+      <el-input v-model="input_inroudoce" placeholder="资源介绍" />
+      <img :src="input_logo_url" style="height: 3rem; width: 3rem" alt="" />
+      <el-button type="" @click="Obtain">获取</el-button>
+      <el-button type="" @click="uploadResource">上传</el-button>
+    </div>
+    <!--    =============-->
+    <div v-for="item in get_datas" :key="item" class="box_container">
+      <h2>{{ item.category }}</h2>
       <div class="box_container_one">
-        <div v-for="item in 14" :key="item" class="box_container_two">
-          <img
-            src="http://127.0.0.1:5000/gallery/20220801-f30a0e61-63d5-4c31-b93d-b2c3c06bb2b9.jpg"
-            alt=""
-          />
-          <span>哔哩哔哩</span>
-          <span>一个学习的网站一个学习的网站一个学习的网站一个学习的网站</span>
+        <div v-for="items in item.arc" :key="items" class="box_container_two">
+          <a :href="items.url">
+            <img :src="items.logourl" alt="" />
+            <span>{{ items.resourcename }}</span>
+            <span>{{ items.intrudoce }}</span>
+          </a>
         </div>
       </div>
     </div>
@@ -25,6 +41,92 @@
 
 <script lang="ts" setup>
 import SecondaryBg from '@/components/SecondaryBg/SecondaryBg.vue'
+import { getResources, uploadResources } from '@/api/resource'
+const input_url = ref('')
+const input_type = ref('')
+const input_name = ref('')
+const input_inroudoce = ref('')
+const input_logo_url = ref('')
+const options = [
+  {
+    value: '学习资源',
+    label: '学习资源'
+  },
+  {
+    value: '图片资源',
+    label: '图片资源'
+  },
+  {
+    value: '办公资源',
+    label: '办公资源'
+  },
+  {
+    value: '娱乐资源',
+    label: '娱乐资源'
+  },
+  {
+    value: '搜索资源',
+    label: '搜索资源'
+  },
+  {
+    value: '设计资源',
+    label: '设计资源'
+  },
+  {
+    value: '工具资源',
+    label: '工具资源'
+  },
+  {
+    value: '音乐专区',
+    label: '音乐专区'
+  },
+  {
+    value: '站长工具',
+    label: '站长工具'
+  }
+]
+const esource_data: any = reactive<any>({
+  type_one: '',
+  type_two: '',
+  url: '',
+  intrudoce: '',
+  logourl: '',
+  resourcename: ''
+})
+const Obtain = () => {
+  input_logo_url.value = input_url.value + '/favicon.ico'
+  // input_logo_url.value = 'https://tools.ly522.com/ico/favicon.php?url=' + input_url.value
+}
+// 上传
+const uploadResource = () => {
+  esource_data.type_one = input_type.value
+  esource_data.type_two = 'null'
+  esource_data.url = input_url.value
+  esource_data.intrudoce = input_inroudoce.value
+  esource_data.logourl = input_logo_url.value
+  esource_data.resourcename = input_name.value
+  const { type_one, type_two, url, intrudoce, logourl, resourcename } = esource_data
+  uploadResources({ type_one, type_two, url, intrudoce, logourl, resourcename }).then(
+    (result: any) => {
+      console.log(result)
+      if (result.result == 200) {
+        ElMessage({
+          message: '上传成功！',
+          type: 'success'
+        })
+      }
+    }
+  )
+}
+let get_datas: any = ref<any>([])
+// 获取
+getResources().then((result: any) => {
+  if (result.result == 200) {
+    const { response } = result
+    console.log(response)
+    get_datas.value = response
+  }
+})
 </script>
 
 <style lang="less" scoped>
